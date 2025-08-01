69e52b1d9e3cbaad6a831e02604b2054a0cc5942
hyunyong <hyunyong.kim@daum.net>
2025-08-01 13:06:03 +0900

    [feature] Step8

--- test_booking_scheduler.py
+++ test_booking_scheduler.py
@@ -3,7 +3,7 @@ from datetime import datetime, timedelta
 from schedule import Customer, Schedule
 from communication import SmsSender, MailSender
 from booking_scheduler import BookingScheduler
-from test_communication import TestableSmsSender
+from test_communication import TestableSmsSender, TestableMailSender
 
 CAPACITY_PER_HOUR = 3
 UNDER_CAPACITY = 1
@@ -76,8 +76,17 @@ def test_예약완료시_SMS는_무조건_발송(booking_scheduler_with_sms_mock
     # assert
     assert sms_mock.send_called
 
-def test_이메일이_없는_경우에는_이메일_미발송():
-    pass
+def test_이메일이_없는_경우에는_이메일_미발송(booking_scheduler):
+    # arrange
+    mail_sender = TestableMailSender()
+    booking_scheduler.set_mail_sender(mail_sender)
+    schedule = Schedule(ON_THE_HOUR, UNDER_CAPACITY, CUSTOMER)
+
+    # act
+    booking_scheduler.add_schedule(schedule)
+
+    # assert
+    assert mail_sender.send_mail_count == 0
 
 def test_이메일이_있는_경우에는_이메일_발송():
     pass
